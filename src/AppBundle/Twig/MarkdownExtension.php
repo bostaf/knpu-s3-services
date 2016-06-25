bef616f1f56a00cd28c1967a035bab1ed534e16a
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-06-25 14:50:15 +0200

    Add twig extension

---
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * User: bostaf
+ * Date: 25.06.16
+ * Time: 14:31
+ */
+
+namespace AppBundle\Twig;
+
+use AppBundle\Service\MarkdownTransformer;
+
+class MarkdownExtension extends \Twig_Extension
+{
+    private $markdownTransformer;
+
+    public function __construct(MarkdownTransformer $markdownTransformer)
+    {
+
+        $this->markdownTransformer = $markdownTransformer;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'), [
+                'is_safe' => ['html']
+            ])
+        ];
+    }
+
+    public function parseMarkdown($str)
+    {
+        return $this->markdownTransformer->parse($str);
+    }
+
+    public function getName()
+    {
+        return 'app_markdown';
+    }
+
+}
